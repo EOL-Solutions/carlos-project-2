1a8e60f3835168fd017ce8288edd18e1b0ab195e
scarlos723 <carlossde58@gmail.com>
2023-02-10 03:33:40 +0000

    add fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './routes/App'
 import './index.css'
 import { RecoilRoot } from 'recoil'
+import Loading from './components/Loading'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Suspense fallback={<div><h2>Loading . . .</h2></div>}>
+  <Suspense fallback={<Loading/>}>
     <RecoilRoot>
       <App />
     </RecoilRoot>
